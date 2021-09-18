1c2d3369105d7c66b643844a5a4708ba1df653a7
shreshth-06 <shreshthchaturvedi2401@gmail.com>
2021-09-18 18:32:57 +0530

    added layout management and created title

--- Desktop/_projects/box-office/src/App.js
+++ Desktop/_projects/box-office/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Navs from './components/Navs';
+
 import Home from './components/pages/Home';
 import Starred from './components/pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
 
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <div>Not Found</div>
-      </Switch>
-    </div>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <div>Not Found</div>
+    </Switch>
   );
 }
 
--- Desktop/_projects/box-office/src/components/pages/Home.js
+++ Desktop/_projects/box-office/src/components/pages/Home.js
@@ -1,8 +1,9 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import MainPageLayout from '../MainPageLayout';
 
 const Home = () => {
-  return <div>This is Home.</div>;
+  return <MainPageLayout>This is Home.</MainPageLayout>;
 };
 
 export default Home;
