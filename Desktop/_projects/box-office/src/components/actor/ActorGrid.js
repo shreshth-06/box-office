a8e9e56c1341fc4bfc108020ba9f523f8410fdf7
shreshth-06 <shreshthchaturvedi2401@gmail.com>
2021-09-27 19:29:59 +0530

    created preview cards for api data

---
+++ Desktop/_projects/box-office/src/components/actor/ActorGrid.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import ActorCard from './ActorCard';
+import IMAGE_NOT_FOUND from '../../images/not-found.png';
+
+const ActorGrid = ({ data }) => (
+  <div>
+    {data.map(({ person }) => (
+      <ActorCard
+        key={person.id}
+        name={person.name}
+        country={person.country ? person.country.name : null}
+        birthay={person.birthday}
+        deathday={person.deathday}
+        gender={person.gender}
+        image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
+      />
+    ))}
+  </div>
+);
+
+export default ActorGrid;
--- Desktop/_projects/box-office/src/pages/Home.js
+++ Desktop/_projects/box-office/src/pages/Home.js
@@ -2,7 +2,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable arrow-body-style */
 import React, { useState } from 'react';
+import ActorGrid from '../components/actor/ActorGrid';
 import MainPageLayout from '../components/MainPageLayout';
+import ShowGrid from '../components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
@@ -37,11 +39,11 @@ const Home = () => {
     }
 
     if (results && results.length > 0) {
-      return results[0].show
-        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-        : results.map(item => (
-            <div key={item.person.id}>{item.person.name}</div>
-          ));
+      return results[0].show ? (
+        <ShowGrid data={results} />
+      ) : (
+        <ActorGrid data={results} />
+      );
     }
     return null;
   };
