e9aec12f09e3d4ed7e4f81bc5dd49397d5f26563
shreshth-06 <shreshthchaturvedi2401@gmail.com>
2021-09-27 17:17:51 +0530

    added search option

--- Desktop/_projects/box-office/src/components/pages/Starred.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable arrow-body-style */
-import React from 'react';
-import MainPageLayout from '../MainPageLayout';
-
-const Starred = () => {
-  return <MainPageLayout>This is Starred.</MainPageLayout>;
-};
-
-export default Starred;
